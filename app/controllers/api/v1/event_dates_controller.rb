e923b376baed7eef457fc149dd23fc3df7951c9b
bitecroissant <bitecroissant@hotmail.com>
2025-01-27 09:09:36 +0800

    🥖 实现分页

---
+++ app/controllers/api/v1/event_dates_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::EventDatesController < ApplicationController
+
+  def index
+		event_dates = EventDate.page(params[:page]).per(10)
+		render json: { resource: event_dates, pager: {
+      page: params[:page] || 1,
+      per_page: event_dates.size(),
+      count: EventDate.count()
+    } }
+  end
+
+    def create
+      event_date = EventDate.new name: :xxxxxxxxxxxxxxxx
+      if event_date.save
+        render json: { resource: event_date }
+      else
+        render errors: { errors: event_date.errors }
+      end
+    end
+end
