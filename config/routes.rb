7df6df64ee41e45d18a0cd2fcd29b92203f7f83b
bitecroissant <bitecroissant@hotmail.com>
2025-01-27 15:50:09 +0800

    🥢 测试创建验证码可以创建

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :event_dates
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
     end
   end
 end
